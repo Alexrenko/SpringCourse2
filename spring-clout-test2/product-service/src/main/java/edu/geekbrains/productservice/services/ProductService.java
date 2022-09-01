71f7a0f8372b64e4112128f73335f6ab55a00b5c
AlexRenko <alexrenko@yahoo.com>
2022-09-01 09:26:03 +0500

    Spring cloud test project

---
+++ spring-clout-test2/product-service/src/main/java/edu/geekbrains/productservice/services/ProductService.java
@@ -0,0 +1,24 @@
+package edu.geekbrains.productservice.services;
+
+import edu.geekbrains.productservice.entities.Product;
+import edu.geekbrains.productservice.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+}
